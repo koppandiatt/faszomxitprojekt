8649f71f6291727d403f59e23e0a0ff83e84b247
Kovacs Z. Levente Botond <kovacs.Levente@expertsolution.ro>
2015-12-02 16:44:49 +0200

    update2

--- Certificari/MainWindow.xaml.cs
+++ Certificari/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Certificari.Views;
+using System.Data;
 
 namespace Certificari
 {
@@ -21,9 +22,13 @@ namespace Certificari
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private DataTable TestBinding;
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void MenuUnitate_Click(object sender, RoutedEventArgs e)
@@ -38,5 +43,50 @@ namespace Certificari
             Candidat candidat = new Candidat();
             candidat.ShowDialog();
         }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            nomenclatorErrortblock.Visibility = Visibility.Collapsed;
+            ///TODO fetch datas from DB
+
+            TestBinding = new DataTable();
+            TestBinding.Columns.Add("Name",typeof(string));
+            TestBinding.Columns.Add("Path",typeof(string));
+            TestBinding.Rows.Add("Alma", "ASdasdsada");
+            TestBinding.Rows.Add("sadasd", "ASdasdsada");
+            TestBinding.Rows.Add("kutya", "ASdasdsada");
+            GridDocumentList.ItemsSource = TestBinding.DefaultView;
+            
+
+        }
+
+        private void Add_document(object sender, RoutedEventArgs e)
+        {
+            nomenclatorErrortblock.Visibility = Visibility.Collapsed;
+            string docSrc = docSrctxtbox.Text;
+            string docName = docNametxtbox.Text;
+
+            try
+            {
+                if (docSrc == String.Empty || docSrc.Trim() == "")
+                {
+                    throw new Exception("Empty document source path!");
+                }
+                if (docName == String.Empty || docName.Trim() == "")
+                {
+                    throw new Exception("Empty document Name!");
+                }
+                TestBinding.Rows.Add(docName, docSrc);
+                //TODO Update to database
+
+            }
+            catch (Exception ex)
+            {
+                nomenclatorErrortblock.Visibility = Visibility.Visible;
+                nomenclatorErrortblock.Text = ex.Message;
+            }
+        }
+
+       
     }
 }
